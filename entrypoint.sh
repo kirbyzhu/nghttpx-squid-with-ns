864e0cd1b8cf1e19215e7cacc3c03b2900ec3098
kirbyzhu <kirbyzhu@gmail.com>
2017-06-07 22:42:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
+ETH=$(eval "ifconfig | grep 'eth0'| wc -l")
+if [ "$ETH"  ==  '1' ] ; then
+	nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
+fi
+if [ "$ETH"  ==  '0' ] ; then
+    nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
+fi
+
 echo -e "$SERVER_CRT" > /config/server.crt
 echo -e "$SERVER_KEY" > /config/server.key
 squid -s -f /config/squid.conf
